dbf607feb818e02b4c037003e21abb3dc8f42f5f
BCT15 <bestbestw34@gmail.com>
2026-09-20 19:58:12 +0700

    fix: adjust correct project folder structure

---
+++ code/src/main/java/com/battleiq/BattleIqApplication.java
@@ -0,0 +1,13 @@
+package com.battleiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleIqApplication.class, args);
+	}
+
+}
